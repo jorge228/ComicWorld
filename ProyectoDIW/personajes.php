8f30908721686f9440b27c97d36edfb2279bb790
Daniapw <daniruiz51@gmail.com>
2019-11-06 14:53:55 +0100

    Personajes

---
+++ ProyectoDIW/personajes.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<?php include("includes/a_config.php"); ?>
+<html lang="en">
+<head>
+    <?php include("includes/contenidoHead.php"); ?>
+
+    <style>
+
+    </style>
+</head>
+
+<body>
+    <!--MENU DE NAVEGACION-->
+    <?php include("includes/menuNav.php"); ?>
+
+    <!--Contenido-->
+    <div class="container-fluid p-0 mt-2">
+
+        <div class="row ">
+            <div class="col-12 text-center">
+                <img class="img-fluid w-100" src="assets/img/carrusel1.jpg"/>
+            </div>
+        </div>
+
+        <div class="row mt-2 text-center">
+            <div class="col-sm-3">
+                <h1>COLUMNA TITULO</h1>
+            </div>
+
+            <div class="col-sm-6">
+                <h1>COLUMNA INFORMACION</h1>
+            </div>
+
+            <div class="col-sm-3">
+                <h1>COLUMNA TABLA DE DATOS</h1>
+            </div>
+        </div>
+    </div>
+    
+    <!--Footer-->
+    <?php include("includes/footer.php"); ?>
+</body>
+</html>
