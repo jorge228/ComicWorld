e4a222a8577ffac10167da26a9f5cf69c04ac778
jorge228 <jorgelozano228@gmail.com>
2019-11-06 14:32:02 +0100

    jorge

---
+++ ProyectoDIW/peliculas.php
@@ -0,0 +1,18 @@
+<?php include "includes/a_config.php";?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include "includes/contenidoHead.php";?>
+    </head>
+    <body>
+
+        <!--MENU DE NAVEGACION-->
+        <?php include "includes/menuNav.php";?>
+
+
+        <!--FOOTER-->
+    <?php include "includes/footer.php";?>
+
+    </body>
+</html>
+
