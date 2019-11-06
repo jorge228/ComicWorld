4623b330ec85aed0dc8689cc7f0f0089b87ee545
jorge228 <jorgelozano228@gmail.com>
2019-11-06 14:55:55 +0100

    nada

--- ProyectoDIW/peliculas.php
+++ ProyectoDIW/peliculas.php
@@ -1,18 +1,30 @@
-<?php include "includes/a_config.php";?>
+<?php include "includes/a_config.php"; ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <?php include "includes/contenidoHead.php";?>
-    </head>
-    <body>
 
-        <!--MENU DE NAVEGACION-->
-        <?php include "includes/menuNav.php";?>
+<head>
+    <?php include "includes/contenidoHead.php"; ?>
+</head>
+
+<body>
+
+    <!--MENU DE NAVEGACION-->
+    <?php include "includes/menuNav.php"; ?>
+
+    <!--CARRUSEL-->
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>--CARRUSEL--</h1>
+            </div>
+        </div>
+
+    </div>
 
 
-        <!--FOOTER-->
-    <?php include "includes/footer.php";?>
+    <!--FOOTER-->
+    <?php include "includes/footer.php"; ?>
 
-    </body>
+</body>
+
 </html>
-
