927705cbc7664d0e59e493746b7eb62c9cbb514c
Daniapw <daniruiz51@gmail.com>
2019-11-07 12:43:00 +0100

    Personajes

--- ProyectoDIW/personajes.php
+++ ProyectoDIW/personajes.php
@@ -17,22 +17,82 @@
     <div class="container-fluid p-0 mt-2">
 
         <div class="row ">
-            <div class="col-12 text-center">
+            <div class="col-12">
                 <img class="img-fluid w-100" src="assets/img/carrusel1.jpg"/>
             </div>
         </div>
 
-        <div class="row mt-2 text-center">
-            <div class="col-sm-3">
-                <h1>COLUMNA TITULO</h1>
+        <div class="row mt-5 mx-4">
+            <div class="col-md-3 border-md-top">
+                <h1 class="text-center font-weight-bold text-primary">BIOGRAFÍA</h1>
             </div>
 
-            <div class="col-sm-6">
-                <h1>COLUMNA INFORMACION</h1>
+            <div class="col-md-6 text-justify">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br> 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.<br>
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
             </div>
 
-            <div class="col-sm-3">
-                <h1>COLUMNA TABLA DE DATOS</h1>
+            <div class="col-md-3 border p-0">
+                <table class="table table-sm">
+                    <thead class="bg-primary">
+                        <tr class="text-center">
+                            <th colspan="2"><h1 class="font-weight-bold">Datos</h1></th>
+                        </tr>
+                    </thead>
+                    <thead class="thead-light">
+                        <tr class="text-center">
+                            <th colspan="2">Datos personales</th>
+                        </tr>
+                    </thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <td>$NombrePers</th>
+                    </tr>
+                    <tr>
+                        <th>Identidad</th>
+                        <td>$NombrePers</th>
+                    </tr>
+                    <tr>
+                        <th>Peso</th>
+                        <td>$NombrePers</th>
+                    </tr>
+                    <tr>
+                        <th>Género</th>
+                        <td>$NombrePers</th>
+                    </tr>
+                    <tr>
+                        <th>Ojos</th>
+                        <td>$NombrePers</th>
+                    </tr>
+                    <tr>
+                        <th>Pelo</th>
+                        <td>$NombrePers</th>
+                    </tr>
+                    <tr>
+                        <th>Edad</th>
+                        <td>$NombrePers</th>
+                    </tr>
+                    <thead class="thead-light">
+                        <tr class="text-center">
+                            <th colspan="2">Cómics</th>
+                        </tr>
+                    </thead>
+                    <tr>
+                        <th>Universo</th>
+                        <td>$NombrePers</th>
+                    </tr>
+                    <tr>
+                        <th>Apodos/aliases</th>
+                        <td>$NombrePers</th>
+                    </tr>
+                    <tr>
+                        <th>Apodos/aliases</th>
+                        <td>$NombrePers</th>
+                    </tr>
+
+                </table>
             </div>
         </div>
     </div>
