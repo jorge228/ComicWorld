0a75786c0606a47dcc7c035f92b87623ed9b8a13
Daniapw <daniruiz51@gmail.com>
2019-11-12 10:26:56 +0100

    Personajes

--- ProyectoDIW/js/pers.js
+++ ProyectoDIW/js/pers.js
@@ -2,10 +2,22 @@ $(function() {
     $('.chart').easyPieChart({
         //Configuration goes here
         size: "150",
-        barColor: 'red',
+        barColor: 'false',
         lineWidth: "20",
-        lineCap:'square'
+        lineCap:'square',
+        trackColor:'#949494',
+        animate:({
+            duration:"1000",
+            enabled:true
+        })
     });
 
+    $(window).scroll(function(){
+        $('.chart').easyPieChart({
+            barColor:'red'
+        });
+
+        $('.chart').data('easyPieChart').update(40);
+    });
 });
 
--- ProyectoDIW/personajes.php
+++ ProyectoDIW/personajes.php
@@ -6,6 +6,8 @@
     include("includes/contenidoHead.php");
 
     include("includes/banners.php");
+
+    include("includes/statsPersonajes.php");
     ?>
 
     <script src='js/pers.js'></script>
@@ -18,50 +20,19 @@
     <!--Contenido-->
     <?php banner("assets/img/carrusel1.jpg");?>
     
-    <div class="container-fluid p-0">
+    <div class="container-fluid p-0" id="contenidoPers">
 
-        <div class="row m-4 text-center">
-            <div class="col-6 col-sm-4 col-md-2 p-0">
-                <div class="chart" id="intel" data-percent="70"><span class="percent">7</span></div>
-                <p class="tituloPuntuacionPer">FUERZA</p>
+        <!--Estadisticas personaje-->
+        <?php estadisticas(7, 10, 8, 9, 8, 8); ?>
 
-            </div>
+        <!--Biografia y datos-->
+        <div class="row mt-4 mx-2">
 
-            <div class="col-6 col-sm-4 col-md-2 p-0">
-                <div class="chart" id="intel" data-percent="80"><span class="percent">8</span></div>
-                <p class="tituloPuntuacionPer">RESISTENCIA</p>
-            </div>
-
-            <div class="col-6 col-sm-4 col-md-2 p-0">
-                <div class="chart" id="intel" data-percent="60"><span class="percent">6</span></div>
-                <p class="tituloPuntuacionPer">INTELECTO</p>
-            </div>
-
-            <div class="col-6 col-sm-4 col-md-2 p-0">
-                <div class="chart" id="intel" data-percent="100"><span class="percent">10</span></div>
-                <p class="tituloPuntuacionPer">AGILIDAD</p>
-            </div>
-
-            <div class="col-6 col-sm-4 col-md-2 p-0">
-                <div class="chart" id="intel" data-percent="100"><span class="percent">10</span></div>
-                <p class="tituloPuntuacionPer">COMBATE</p>
-            </div>
-
-            <div class="col-6 col-sm-4 col-md-2 p-0">
-                <div class="chart" id="intel" data-percent="100"><span class="percent">10</span></div>
-                <p class="tituloPuntuacionPer">VELOCIDAD</p>
-            </div>
-
-        
-        </div>
-
-        <div class="row mt-4 mx-4">
-
-            <div class="col-md-3 border-md-top">
+            <div class="col-md-2 border-md-top pr-3">
                 <h1 class="text-center font-weight-bold text-primary">BIOGRAFÍA</h1>
             </div>
 
-            <div class="col-md-6 text-justify">
+            <div class="col-md-7 px-5">
                 <p>Spider-Man (llamado Hombre Araña en muchas de las traducciones al español) es un superhéroe ficticio creado por los escritores y editores Stan Lee y Steve Ditko. Apareció por primera vez en el cómic de antología Amazing Fantasy # 15 (10 de agosto de 1962), en la Edad de Plata de los cómics. Aparece en los cómics estadounidenses publicados por Marvel Comics, así como en varias películas, programas de televisión y adaptaciones de videojuegos ambientadas en el Universo Marvel. En las historias, Spider-Man es el alias de Peter Parker, un huérfano criado por su tía May y su tío Ben en la Ciudad de Nueva York después de que sus padres Richard y Mary Parker murieron en un accidente aéreo. Lee y Ditko tuvieron que lidiar con los problemas de la adolescencia y los problemas financieros, y lo acompañaron con muchos personajes de apoyo, como J. Jonah Jameson, Flash Thompson, Harry Osborn, los intereses románticos, Gwen Stacy y Mary Jane Watson, y enemigos como el Doctor Octopus, Kingpin, Duende Verde y Venom. Su historia de origen lo tiene adquiriendo habilidades relacionadas con la araña después de un mordisco de una araña radioactiva; estos incluyen aferrarse a las superficies, 
                 disparar telarañas desde dispositivos montados en la muñeca y detectar el peligro con su "sentido arácnido".</p>
 
