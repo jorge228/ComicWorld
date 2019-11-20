9b558aee6e35c99d8158bae07180dfe5ce70b476
Azueon <44552609+Azueon@users.noreply.github.com>
2019-11-20 11:20:20 +0100

    Added Sass Override

--- ProyectoDIW/videojuegos/LegoMarvelSuperheroes.php
+++ ProyectoDIW/videojuegos/LegoMarvelSuperheroes.php
@@ -18,46 +18,69 @@
             </div>
 
             <!-- Fila 1 -->
-            <div class="row mt-3">
-                <div class="col-3">               
-                    <img src="../assets/img/videojuegos_5_001.jpg" style="height: 100px;"/>
+            <div class="row mt-4">
+                <div class="col-3 align-self-center text-center">               
+                    <img class="shadow-lg" src="../assets/img/videojuegos_5_001.jpg" style="height: 200px;"/>
                 </div>
                 <div class="col-9">
-                    <div class="card p-3">
+                    <div class="shadow card p-3">
+                        <p>
+                        <h2 class="text-center">Lego Batman 2: DC Super Héroes</h2>    
+                        Lego Batman 2: DC Super Heroes es un videojuego de Lego y DC comics estrenado en junio de 2012,
+                         y es la secuela de Lego Batman: El Videojuego (2008). Fue desarrollado por Traveller's Tales y
+                        comercializado por Warner Bros. Interactive Entertainment. Hay versiones para PlayStation 3,
+                         PlayStation Vita, Nintendo 3DS, Wii, Nintendo DS, Xbox 360, y Microsoft Windows. Una gran
+                          variedad de personajes de DC Comics dentro y fuera de la continuidad de Batman serán personajes jugables. Lego Batman 2: DC Super Heroes también cuenta con diálogo, el segundo juego Lego de Traveller's Tales en hacerlo.
+                        </p>
+                        <p>Desarrollado por: Traveller's Tales</p>
+                        <p>Fecha lanzamiento: 19 de junio de 2012</p>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </p>
                     </div>
                 </div>
             </div>
 
             <!-- Fila 2 -->
-            <div class="row mt-3">
+            <div class="row mt-4">
                 <div class="col-9">
-                    <div class="card p-3">
+                    <div class="shadow card p-3">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </p>
                     </div>
                 </div>
-                <div class="col-3">
-                    <img src="../assets/img/videojuegos_5_001.jpg" style="height: 100px;"/>
+                <div class="col-3 mx-auto align-self-center text-center">
+                    <img class="shadow-lg" src="../assets/img/videojuegos_5_001.jpg" style="height: 200px;"/>
                 </div>
             </div>
 
             <!-- Fila 3 -->
-            <div class="row mt-3">
-                <div class="col-3">               
-                    <img src="../assets/img/videojuegos_5_001.jpg" style="height: 100px;"/>
+            <div class="row mt-4">
+                <div class="col-3 align-self-center text-center">               
+                    <img class="shadow-lg" src="../assets/img/videojuegos_5_001.jpg" style="height: 200px;"/>
                 </div>
                 <div class="col-9">
-                    <div class="card p-3">
+                    <div class="shadow card p-3">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </p>
                     </div>
                 </div>
             </div>
