55ca211052aa066fc65fe4868c2f297361703510
Daniapw <daniruiz51@gmail.com>
2019-11-21 20:38:23 +0100

    Personajes

--- ProyectoDIW/includes/inclPersonajes/banners.php
+++ ProyectoDIW/includes/inclPersonajes/banners.php
@@ -4,10 +4,13 @@ function banner($rutaImagen){?>
 
     <div class="container-fluid ">
         <div class="row ">
-            <div class="col-12 p-0">
-                <div class="overlayBannerPers bg-primary">
-                    <img class="img-fluid w-100" src="<?php echo $rutaImagen ?>"/>
+            <div class="col-12 p-0 bannerPers">
+                <div class="overlayBannerPers d-none d-md-block">
+                    <div class="d-flex justify-content-center align-items-center pb-5 h-100  ">
+                        <h1 class="tituloPers text-weight-bold text-white">SPIDERMAN</h1>
+                    </div>
                 </div>
+                
             </div>
         </div>
     </div>
