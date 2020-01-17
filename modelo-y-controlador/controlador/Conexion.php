f4cdfd86b03dfdb35f817768ac3b8f53b7e2e433
Daniapw <daniruiz51@gmail.com>
2020-01-17 14:48:23 +0100

    Modelo y controlador

---
+++ modelo-y-controlador/controlador/Conexion.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Conexion
+ *
+ * @author DWES
+ */
+class Conexion extends mysqli{
+    private $server='localhost';
+    private $usuario='dwes';
+    private $pass='abc123.';
+    private $bdd='comicworld';
+    
+    public function __construct() {
+        parent::__construct($this->server, $this->usuario, $this->pass, $this->bdd);
+        mysqli_set_charset($this, "utf8");
+    }
+}
