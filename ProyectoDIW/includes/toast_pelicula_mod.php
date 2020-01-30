19a419ccbda356408fb0f76d8b60b729c90bc94f
jorge228 <jorgelozano228@gmail.com>
2020-01-30 17:15:58 +0100

    toast pelis

---
+++ ProyectoDIW/includes/toast_pelicula_mod.php
@@ -0,0 +1,27 @@
+<?php
+function imprimeToast($opcion)
+{
+?>
+    <script>
+        toastr.options = {
+            "closeButton": true,
+            "debug": false,
+            "newestOnTop": false,
+            "progressBar": false,
+            "positionClass": "toast-top-right",
+            "preventDuplicates": false,
+            "onclick": null,
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": "7000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        }
+        toastr["success"]("Película <?php echo $opcion; ?> correctamente", "Correcto");
+    </script>
+<?php
+}
+?>
