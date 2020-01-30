a89df8adeb1603d1f508d5b08b59c753ffe34ad7
Azueon <44552609+Azueon@users.noreply.github.com>
2020-01-30 01:11:58 +0100

    Login Registro Estables
    Login Registro Estables y barra navegacion modificada. Mejora contra inyeccion SQL

---
+++ ProyectoDIW/js/validacion_form/form_signUp.js
@@ -0,0 +1,14 @@
+$(function(){
+    $("form.needs-validation").submit(function (e) {
+        
+        if(this.checkValidity()===false){
+
+            e.preventDefault();
+            e.stopPropagation();
+
+            if (!$(this).hasClass("was-validated"))
+                $(this).toggleClass("was-validated");
+        }
+    });
+
+})
