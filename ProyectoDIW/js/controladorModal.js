488a273cd37e650fd90682dc4a5bf5a8e345f08e
Daniapw <daniruiz51@gmail.com>
2020-03-03 14:13:14 +0100

    Arreglo boton google

--- ProyectoDIW/js/controladorModal.js
+++ ProyectoDIW/js/controladorModal.js
@@ -15,14 +15,7 @@ $(function() {
         e.preventDefault();      
     });
 
-    $("#exampleModal").keypress(function (e) {      
-        if (e.which == 13) {
-            $('#btnEntrar').click();
-            e.preventDefault();
-        }
-    }); 
     //  Fin Control Eventos:
-
     if ($('#errorMode').length) {
         $('#usuario').css('background-color', "#FFDDDD");
         $('#password').css('background-color', "#FFDDDD");
